b9718081c733b851020d76e3e87157dea58fed3a
projectbitnest <johnabruzzio@outlook.com>
2025-06-29 12:59:39 +0100

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Dashboard() {
+  return <div>User Dashboard with balance and deposit options</div>;
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to BitNest</h1>
+      <p>The most trusted crypto investment platform with over 1 million users.</p>
+    </div>
+  );
+}
